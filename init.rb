41e9292b6d660b66c176a0f10b9612b937c79184
Steffen Schmidt <s.schmidt@s-v.de>
2013-07-15 12:38:17 +0200

    Generic plugin and author settings

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :dashboard do
   name 'Dashboard plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Steffen Schmidt'
+  description 'This plugin allow you to define which information should be displayed on the dashboard page for different user groups.'
+  version '0.1'
+  url 'https://github.com/steffenschmidt/redmine-dashboard'
+  author_url 'https://github.com/steffenschmidt'
 end
